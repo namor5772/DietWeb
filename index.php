0b99440d4c380785972af656dba0a460f2b10ddc
roman groblicki <romangroblicki@proton.me>
2023-12-09 12:41:57 +1100

    Laptop/wamp64 9/12/2023 12:41

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
 
 <body>
 
-<?php
+    <?php
     $firstName = "John";
     $lastName ="Doe";
     print "<p>Hello $firstName $lastName!</p>";
-?>
+    ?>
+    <a href="DietSentry.zip" download> Diet Sentry 2.0 zip file</a>
 
 </body>
 </html>
