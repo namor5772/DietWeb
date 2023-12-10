302c8411ad41d6d820347e49bdd5f0490417485e
roman groblicki <romangroblicki@proton.me>
2023-12-10 14:54:04 +1100

    Laptop/wamp64 10/12/2023 14:53

--- index.php
+++ index.php
@@ -3,16 +3,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Diet Sentry</title>
+    <title>Diet Sentry 2.0</title>
     <style type="text/css">
         h1 {
             font: 36pt "arial Black";
             text-align: center;
             background-color: aquamarine;
         }
+
         h2 {
             font: 18pt "arial Black";
         }
+
+        h3 {
+/*            font: 36pt "arial Black";  */
+            text-align: center;
+            color: aquamarine;
+            height: 58px;
+        }
+    
         li { 
             font: 12pt "arial"; 
         }
@@ -23,17 +32,59 @@
             max-width: 90%;
             height: auto;
             border: 5px solid aquamarine;
-        }        
+        }
+        img#special {
+            max-width: 100%;
+            height: auto;
+            border: 0;
+        }
+        p#special {
+            font: 36pt "arial Black";
+            text-align: center;
+            background-color: aquamarine;
+        }
+
+        .container {
+            display: flex;
+            align-items: center;
+            background-color: aquamarine;
+            height: 68px;
+        }
+
+        .container img {
+            background-color: aquamarine;
+        }
+
+        #special2 {
+            height: 58px;
+        }
+
+        
     </style>
+    <link rel="icon" href="DScarrot.ico">
 </head>
 
 <body>
-    <!-- Write your comments here -->
-    <h1>Diet Sentry 2.0</h1>
-    <p>This is the website presence for the standalone Windows Forms based diet monitoring application called DietSentry 2.0. It enables people to download and install it onto their own Windows computer.</p>
-    <p>It contains an Australian based foods database with around 1800 foods using the sensible units, kJ, g, mg and mL, and the d-MMM-yy HH:mm DateTime format. It also contains selected commercial foods based on their mandated Nutritional Information Panels.</p>
+    <?php
+    ;
+    ?>
+
+<!--   <img id="special" src="DScarrot.ico" /> */
+        <h1>Diet Sentry 2.0</h1>;
+                height: 1em;
+-->
+    <div class="container">
+        <p><h3>...</h3></p>
+        <img id="special2" src="DScarrot.ico">
+        <p><h3>...</h3></p>
+        <p><h1>Diet Sentry 2.0</h1></p>
+        <p><h3>...</h3></p>
+    </div>
+
+    <p>This is the website presence for the completely free standalone Windows Forms based diet monitoring application called Diet Sentry 2.0. Here you can download and install it onto your own Windows computer.</p>
+    <p>It contains an Australian based foods database with around 1800 foods using the sensible units kJ, g, mg and mL, and the logical  d-MMM-yy HH:mm  DateTime format. It also contains selected commercial foods based on their mandated Nutritional Information Panels.</p>
     <p>When a food is consumed it is selected and an amount eaten entered. This appends the details to a DateTime stamped record. The cumulative totals of the daily Energy and nutrients can then be monitored.</p>
-    <p>New foods can be manually added to the database. For convenience it is also possible to add a recipe to the foods database. Of course foods can be deleted and edited, this includes eaten foods.</p>
+    <p>New foods can be manually added to the database. For convenience it is also possible to add a recipe to the foods database. Foods can also be deleted and edited, including eaten foods.</p>
     <p>The application source can be cloned into Visual Studio for compiling from the git repository <a href="https://github.com/namor5772/DietSentry">https://github.com/namor5772/DietSentry</a> </p>
 <hr/>
     <a href="DietSentry.zip" download> <h2>Download</h2> </a>
@@ -59,5 +110,6 @@
         Main Form
     </p>
         <img src="Main2.jpg" alt="The applications main form" />
+<hr/>
 </body>
 </html>
